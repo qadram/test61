d5ca4da1c532f95dadb6ef9870462ece0b4afdab
qadram <qadram@gmail.com>
2023-10-24 15:52:51 +0200

    Add files via upload

---
+++ Unit1.pas
@@ -0,0 +1,60 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, JS, Web, WEBLib.Graphics, WEBLib.Controls,
+  WEBLib.Forms, WEBLib.Dialogs, WEBLib.Menus, WEBLib.StdCtrls;
+
+
+type
+  TForm1 = class(TWebForm)
+    WebButton1: TWebButton;
+    WebSpinEdit1: TWebSpinEdit;
+    WebEdit1: TWebEdit;
+    WebEdit2: TWebEdit;
+    WebDateTimePicker2: TWebDateTimePicker;
+    procedure Form1Create(Sender: TObject);
+    procedure Form1Deactivate(Sender: TObject);
+    procedure WebButton1Click(Sender: TObject);
+    procedure WebListBox1Click(Sender: TObject);
+    procedure WebMemo1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Web Memo1Click(Sender: TObject);
+begin
+  
+end;
+
+procedure TForm1.WebListBox1Click(Sender: TObject);
+begin
+  
+end;
+
+procedure TForm1.WebButton1Click(Sender: TObject);
+begin
+  
+end;
+
+procedure TForm1.Form1Deactivate(Sender: TObject);
+begin
+  
+end;
+
+procedure TForm1.Form1Create(Sender: TObject);
+begin
+  
+end;
+
+end.                                         
